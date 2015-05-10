969aeec3ee9dac5f01dea00ae8fcdc0b57b6611b
Gerson Scanapieco <gerson.scanapieco@gmail.com>
2015-05-10 14:40:37 -0300

    Add gems para testes
    - Add gem shoulda-matchers
    - Add gem capybara para testes de integracao
    - Add gem database_cleaner para limpar o banco de dados apos
      cada teste. Add arquivo spec/support/database_cleaner.rb com
      configuracoes para testes normais e com flag :js
    - Add gem selenium-webdriver para utilizar o selenium como
      driver do capybara em testes com javascript

--- Gemfile
+++ Gemfile
@@ -36,15 +36,6 @@ gem 'simple_form'
 gem 'devise', '~> 3.4.1'
 gem 'devise-i18n'
 
-# Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
-
-# Use Unicorn as the app server
-# gem 'unicorn'
-
-# Use Capistrano for deployment
-# gem 'capistrano-rails', group: :development
-
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
@@ -63,3 +54,9 @@ group :development, :test do
   gem 'rack_session_access'
 end
 
+group :test do
+  gem 'shoulda-matchers', require: false
+  gem 'capybara', '~> 2'
+  gem 'database_cleaner'
+  gem 'selenium-webdriver'
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,9 @@
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
+require "rack_session_access/capybara"
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
@@ -42,14 +45,15 @@ RSpec.configure do |config|
 
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
-=begin
+
   # These two settings work together to allow you to limit a spec run
   # to individual examples or groups you care about by tagging them with
   # `:focus` metadata. When nothing is tagged with `:focus`, all examples
   # get run.
   config.filter_run :focus
   config.run_all_when_everything_filtered = true
-
+  
+=begin
   # Limits the available syntax to the non-monkey patched syntax that is
   # recommended. For more details, see:
   #   - http://myronmars.to/n/dev-blog/2012/06/rspecs-new-expectation-syntax
