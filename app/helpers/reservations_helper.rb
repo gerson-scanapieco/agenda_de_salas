ad7a0d1d5be1cc8be12f37cc24c9798f008d23a7
Gerson Scanapieco <gerson.scanapieco@gmail.com>
2015-05-11 00:38:27 -0300

    Add helper para deixar view Reservations#index mais legivel
    - Metodo reservation_entry de ReservationsHelper encapsula a logica
      de criar uma entrada pra uma Reserva na tela.
    - Add specs para o novo helper

--- app/helpers/reservations_helper.rb
+++ app/helpers/reservations_helper.rb
@@ -1,2 +1,18 @@
 module ReservationsHelper
+
+  def reservation_entry(reservations, time, date)
+    reservation = reservations.get_reservation_by_time_and_date(time, date)
+
+    unless reservation
+      return link_to 'Reservar', '#', class: 'create-reservation-link'
+    end
+
+    if reservation.user == current_user
+      link_to "Reservado para #{ reservation.user_email }", '#', 
+                class: 'destroy-reservation-link', data: { id: reservation.id }
+    else
+      "Reservado para #{ reservation.user_email }"
+    end
+  end
+
 end
--- spec/helpers/reservations_helper_spec.rb
+++ spec/helpers/reservations_helper_spec.rb
@@ -1,15 +1,43 @@
 require 'rails_helper'
 
-# Specs in this file have access to a helper object that includes
-# the ReservationsHelper. For example:
-#
-# describe ReservationsHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
 RSpec.describe ReservationsHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "reservation_entry(reservations, time, date)" do
+    context "when theres no Reservation for given time and date" do
+      let(:reservations_array) { [] }
+
+      it 'returns a link with text "Reservar"' do
+        expect(helper.reservation_entry(reservations_array, 6, Date.today)).to eq link_to 'Reservar', '#', class: 'create-reservation-link'
+      end
+    end
+
+    context "when the Reservation found belongs to the current user" do
+      let(:reservation) { create(:reservation, :with_user, time: 6, date: Date.today) }
+      let(:user) { reservation.user }
+      let(:reservations_array) { [ reservation ] }
+
+      before(:each) do
+        allow(helper).to receive_messages(current_user: user)
+      end
+
+      it 'returns a link with the text "Reservado para :current_user_email"' do
+        expect(helper.reservation_entry(reservations_array, 6, Date.today)).to eq link_to "Reservado para #{ reservation.user_email }", '#', 
+                class: 'destroy-reservation-link', data: { id: reservation.id }
+      end
+    end
+
+    context "when the Reservation found belongs to another user" do
+      let(:reservation) { create(:reservation, :with_user, time: 6, date: Date.today) }
+      let(:user) { create(:user) }
+      let(:reservations_array) { [ reservation ] }
+
+      before(:each) do
+        allow(helper).to receive_messages(current_user: user)
+      end
+
+      it 'returns the text "Reservado para :another_user_email"' do
+        expect(helper.reservation_entry(reservations_array, 6, Date.today)).to eq "Reservado para #{ reservation.user_email }"
+      end
+    end
+
+  end
 end
